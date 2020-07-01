9a0281b6f0355fce79d24291ca867dec03f4edd0
INEI <yyyy5711@yahoo.co.jp>
2020-07-01 15:29:12 +0900

    add RequestMapping

--- demo/src/main/java/com/example/demo/Democontroller.java
+++ demo/src/main/java/com/example/demo/Democontroller.java
@@ -2,17 +2,19 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dev")
 public class Democontroller {
 	
-	@RequestMapping("/out")
+	@GetMapping("/out")
 	public String out() {
-		return "hello.html";
-		
+		return "hello.html";	
 	}
-	@RequestMapping("/list")
+	
+	@GetMapping("/list")
 	public String result(Model model) {
 
 		model.addAttribute("name","daniel");
